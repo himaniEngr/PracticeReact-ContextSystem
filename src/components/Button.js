51b86e0ec22454e6832a5c34c47b4b6fa0b935c2
Himani <himani9.engg@gmail.com>
2022-02-08 13:33:00 +0530

    accessing data with consumers

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,19 @@ import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
   static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 export default Button;
